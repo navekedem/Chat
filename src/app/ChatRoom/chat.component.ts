de61cb116bca979e71fc9a3bbffdec7d4af18b1e
navekedem <navekedem@gmail.com>
2019-02-21 02:47:28 +0200

    Upload-App

--- src/app/ChatRoom/chat.component.ts
+++ src/app/ChatRoom/chat.component.ts
@@ -18,6 +18,7 @@ export class ChatComponent implements OnInit {
   messages: Message[] = [];
 
   constructor(private chatService: ChatService){
+    
     this.chatService.updateClientsOnNewMessage().subscribe(newMessage => {
       this.messages.push(newMessage);
     });
